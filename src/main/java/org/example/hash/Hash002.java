48d9961d8a3a54d0a47a77b9abcd7562ac10cb1b
jade <ahyz0569@gmail.com>
2024-04-20 18:43:18 +0900

    refactor: 주석 추가 및 메서드 파라미터 추가

--- src/main/java/org/example/hash/Hash002.java
+++ src/main/java/org/example/hash/Hash002.java
@@ -4,14 +4,19 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/**
+ * 문제 : 완주하지 못한 선수
+ * 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/42576
+ */
 public class Hash002 {
-    public String solution() {
-        String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        String[] completion = {"stanko", "ana", "mislav"};
+    public String solution(String[] participant, String[] completion) {
         String answer = "";
 
-        Map<String, Long> partMap = Arrays.stream(participant).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        Map<String, Long> compMap = Arrays.stream(completion).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<String, Long> partMap = Arrays.stream(participant)
+                                          .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        Map<String, Long> compMap = Arrays.stream(completion)
+                                          .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
         for (Map.Entry<String, Long> partEntry : partMap.entrySet()) {
             if (!compMap.containsKey(partEntry.getKey())) return partEntry.getKey();
--- src/main/java/org/example/hash/Hash003.java
+++ src/main/java/org/example/hash/Hash003.java
@@ -2,16 +2,20 @@ package org.example.hash;
 
 import java.util.*;
 
+/**
+ * 문제: 전화번호 목록
+ * 출처: https://school.programmers.co.kr/learn/courses/30/lessons/42577
+ * */
 public class Hash003 {
-    public boolean solution() {
-//        String[] phone_book = {"119", "97674223", "1195524421"};
-        String[] phone_book = {"123", "12345", "1245", "1243", "12"};
-//        Arrays.sort(phone_book, Comparator.comparingInt(String::length).reversed());
+    public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
 
         for (int i = 0; i < phone_book.length; i++) {
-            if (i == phone_book.length -1 ) return true;
-            if (phone_book[i+1].startsWith(phone_book[i])) return false;
+            if (i == phone_book.length -1 )
+                return true;
+
+            if (phone_book[i+1].startsWith(phone_book[i]))
+                return false;
         }
 
         return true;
