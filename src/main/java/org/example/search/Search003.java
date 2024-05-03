4f9be5ad6850bea1df15b38be61f7b64c4046623
jade <ahyz0569@gmail.com>
2024-05-03 23:04:46 +0900

    feat: 완전탐색 문제 3(소수 찾기) 문제 풀이 2

--- src/main/java/org/example/search/Search003.java
+++ src/main/java/org/example/search/Search003.java
@@ -1,77 +1,50 @@
 package org.example.search;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.stream.Collectors;
 
 /**
  * 완전탐색 유형 문제 : 소수 찾기
  * 출처: https://school.programmers.co.kr/learn/courses/30/lessons/42839
- * */
+ */
 public class Search003 {
+    static String[] numberArray;
+    static boolean[] chkArray;
+    static HashSet<Integer> numberSet;
+
     public int solution(String numbers) {
-        char[] charArray = numbers.toCharArray();
-        int[] numberArray = new int[charArray.length];
+        numberSet = new HashSet<>();
+        numberArray = numbers.split("");
 
-        for (int i = 0; i < charArray.length; i++) {
-            numberArray[i] = Character.getNumericValue(charArray[i]);
-        }
-        Arrays.sort(numberArray);
+        chkArray = new boolean[numberArray.length];
 
-        HashSet<Integer> numberSet = new HashSet<>();
-
-        do {
-            String number = Arrays.stream(numberArray).mapToObj(String::valueOf).collect(Collectors.joining(""));
-            numberSet.add(Integer.valueOf(number));
-        } while (nextPermutation(numberArray));
-
-        Arrays.stream(numberArray).boxed().distinct().filter(i -> i > 1).forEach(numberSet::add);
+        permutation(0, "");
 
         // 소수판별 메서드를 통해 각 숫자별 소수 여부 판별하기
-        int answer = 0;
-        for (Integer i : numberSet) {
-            if (isPrime(i)) answer++;
-        }
+        long count = numberSet.stream().filter(this::isPrime).count();
 
-        return answer;
+        return Long.valueOf(count).intValue();
+    }
+
+    public void permutation(int idx, String sum) {
+        if (!sum.isEmpty()) {
+            numberSet.add(Integer.valueOf(sum));
+        }
+        for (int i = 0; i < numberArray.length; i++) {
+            if (!chkArray[i]) {
+                chkArray[i] = true;
+                permutation(idx + 1, sum + numberArray[i]);
+                chkArray[i] = false;
+            }
+        }
     }
 
     public boolean isPrime(int number) {
         if (number < 2) return false;
-        for (int i = 2; i*i <= number; i++) {
-             if (number % i == 0)
-                 return false;
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0)
+                return false;
         }
 
         return true;
-    }
-
-    public boolean nextPermutation(int[] numbers) {
-        int size = numbers.length;
-        int i = size - 1;
-
-        while (i > 0 && numbers[i - 1] >= numbers[i])
-            i--;
-
-        if (i == 0) return false;
-
-        int j = size - 1;
-        while (numbers[i - 1] >= numbers[j])
-            j--;
-
-        swap(numbers, i - 1, j);
-
-        int k = size - 1;
-        while (i < k) {
-            swap(numbers, i++, k--);
-        }
-        return true;
-    }
-
-    public void swap(int[] numbers, int i, int j) {
-        int temp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = temp;
     }
 }
